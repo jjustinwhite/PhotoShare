74e475ea123adc61fdeb83772c6e3ba3168e51d4
Justin White <blinklink11@aol.com>
2012-08-01 21:12:15 -0500

    S3 fix

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,7 +12,7 @@ class Photo < ActiveRecord::Base
         },
         :s3_credentials => { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCES_KEY'], :bucket => "PhotoShare-jw"},
         :storage => :s3,
-        :path => "/:attachment/:id/:style.:extension",
+        :path => "photoshare-jw/:attachment/:id/:style.:extension",
         :bucket => 'PhotoShare-jw'
   #===END===Paperclip===========================================
   
