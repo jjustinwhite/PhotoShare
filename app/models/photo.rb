6591b129846d6f3e3d4c04a453fe9d8f3f73dbe5
Justin White <blinklink11@aol.com>
2012-08-01 20:17:22 -0500

    S3 fix

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
     attr_accessible :name, :image, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+    attr_accessor :image_file_name
     belongs_to :album
   
   #===START===Paperclip===========================================
